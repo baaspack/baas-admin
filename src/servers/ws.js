31ec96a93442d67b9e5a165ef92e6adb2a639f4f
Ido <imidom@gmail.com>
2020-04-05 23:39:13 -0400

    Use a proxy server to connect to a baas via stackname

--- src/servers/ws.js
+++ src/servers/ws.js
@@ -1,8 +1,10 @@
 import WebSocket from 'ws';
+import httpProxy from 'http-proxy';
 
 export const createWsServer = (httpServer, sessionParser) => {
   const wsConnections = new Map();
   const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+
 
   httpServer.on('upgrade', (req, socket, head) => {
     sessionParser(req, {}, () => {
@@ -11,9 +13,31 @@ export const createWsServer = (httpServer, sessionParser) => {
         return;
       }
 
-      wss.handleUpgrade(req, socket, head, (ws) => {
-        wss.emit('connection', ws, req);
-      });
+      const [rootResource, stackName] = req.url.slice(1).split('/');
+
+      if (rootResource === 'backpacks' && stackName) {
+        const stackUrl = `http://${stackName}_backpack:4000`;
+        console.log(`connecting to ${stackUrl}`);
+
+        const proxy = httpProxy.createProxyServer({
+          target: stackUrl,
+          ws: true,
+        });
+
+        proxy.ws(req, socket, head);
+
+        proxy.on('open', () => {
+          console.log(`connected to ${stackName}!`);
+        });
+
+        proxy.on('error', (err) => {
+          console.error(err);
+        });
+      } else {
+        wss.handleUpgrade(req, socket, head, (ws) => {
+          wss.emit('connection', ws, req);
+        });
+      }
     });
   });
 
